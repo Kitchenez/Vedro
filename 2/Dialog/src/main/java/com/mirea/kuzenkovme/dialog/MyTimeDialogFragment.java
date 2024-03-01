78510f9e52ef2c2b877f9ae67f35a2381dd873e9
Kitchenez <WellSoKindWorld@yandex.ru>
2024-03-01 18:38:12 +0300

    Lesson 2

---
+++ 2/Dialog/src/main/java/com/mirea/kuzenkovme/dialog/MyTimeDialogFragment.java
@@ -0,0 +1,29 @@
+package com.mirea.kuzenkovme.dialog;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+import java.util.Calendar;
+
+public class MyTimeDialogFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Создание нового экземпляра TimePickerDialog и возврат его
+        return new TimePickerDialog(getActivity(), this, hour, minute, true);
+    }
+
+    @Override
+    public void onTimeSet(android.widget.TimePicker view, int hourOfDay, int minute) {
+        // Здесь вы можете выполнить действия при установке времени
+    }
+}
+
+
